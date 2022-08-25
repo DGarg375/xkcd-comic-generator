86bd8376e4b7c5f5998cd9bc2fb91d21351f6591
DGarg375 <gargdivyansh11@gmail.com>
2022-08-25 16:25:56 +0530

    added alert

--- client/src/components/Nav/NavButton.js
+++ client/src/components/Nav/NavButton.js
@@ -3,6 +3,11 @@ import './NavButton.css'
 function NavButton(props) {
     function first() {
         console.log(props.navName);
+        if(props.navName === "XKCD OFFICIAL WEBSITE") {
+           if(window.confirm("Are you sure? You are leaving this website...") === true) {
+            window.location = "https://xkcd.com/";
+           }
+        }
     }
     return(
         <button className="navButton" type="button" onClick={first}>
