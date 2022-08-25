d314e0b9ddb76cbe65f86c80863174b448fe786c
DGarg375 <gargdivyansh11@gmail.com>
2022-08-25 16:31:31 +0530

    add timeout function

--- client/src/components/Nav/NavButton.js
+++ client/src/components/Nav/NavButton.js
@@ -4,9 +4,11 @@ function NavButton(props) {
     function first() {
         console.log(props.navName);
         if(props.navName === "XKCD OFFICIAL WEBSITE") {
-           if(window.confirm("Are you sure? You are leaving this website...") === true) {
-            window.location = "https://xkcd.com/";
-           }
+            setTimeout(() => {
+                if(window.confirm("Are you sure? You are leaving this website...") === true) {
+                    window.location = "https://xkcd.com/";
+                }
+            }, 500);
         }
     }
     return(
