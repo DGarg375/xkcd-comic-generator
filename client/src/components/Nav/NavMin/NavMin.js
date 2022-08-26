26da2166f25da2021f08a653345a82929165b787
DGarg375 <gargdivyansh11@gmail.com>
2022-08-27 00:12:05 +0530

    completed mobile view navbar

--- client/src/components/Nav/NavMin/NavMin.js
+++ client/src/components/Nav/NavMin/NavMin.js
@@ -10,7 +10,14 @@ function NavMin() {
                 <div className="dropdown-container">
                     <div className="dropdown">
                         <a href="/"><div className="drop-text">ABOUT</div></a>
-                        <div className="drop-text" id="drop-get-comics">GET COMICS</div>
+                        <div className="dropdown-comic">
+                            <div className="drop-text" id="drop-get-comics">GET COMIC</div>
+                            <div className="hello">
+                                <a href="/"><div className="drop-get-item">RANDOMLY</div></a>
+                                <a href="/"><div className="drop-get-item">BY COMIC ID</div></a>
+                                <a href="/"><div className="drop-get-item">LATEST</div></a>
+                            </div>
+                        </div>
                         <a href="https://xkcd.com/"><div className="drop-text">XKCD WEBSITE</div></a>
                         <a href="/"><div className="drop-text">AUTHOR</div></a>
                     </div>
