df3d33877f13e3f0809e6b6058a4cbb691ee70cb
DGarg375 <gargdivyansh11@gmail.com>
2022-08-28 16:50:25 +0530

    navbar mobile view minor changes; started desktop view complete overhaul

--- client/src/components/Nav/NavMax/NavMax.js
+++ client/src/components/Nav/NavMax/NavMax.js
@@ -3,26 +3,7 @@ import './NavMax.css';
 function NavMax() {
     return(
         <div className="navbar-maximized-window">
-            <a href="/" className="buttonLink">
-                <div className="navbar-max-buttons">
-                    ABOUT
-                </div>
-            </a>
-            <a href="/" className="buttonLink">
-                <div className="navbar-max-buttons">
-                    GET COMICS
-                </div>
-            </a>
-            <a href="https://xkcd.com/" className="buttonLink">
-                <div className="navbar-max-buttons">
-                    XKCD WEBSITE
-                </div>
-            </a>
-            <a href="/" className="buttonLink">
-                <div className="navbar-max-buttons">
-                    AUTHOR
-                </div>
-            </a>
+            vvvf
         </div>
     );
 }
