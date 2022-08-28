db9d31839b3020c68a7fe188df9415999d58822f
DGarg375 <gargdivyansh11@gmail.com>
2022-08-28 15:19:19 +0530

    minor changes to mobile view drop-down

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,62 +8,6 @@ function App() {
       <Nav />
       <br></br><br></br><br></br>
       {/* New stuff */}
-      cbdif<br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      hiorhfior
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      hfoehfef
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      hfioehfoiefhe
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      bceufef
-      <br></br>
-      bjfefe
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      jfefhefvdbjvd
-      <br></br>
-      <br></br>
-      <br></br>
-      bjcdbjeveve
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      vhiofnvoreifnreo
-      <br></br>
-      <br></br>
-      <br></br>
-      chodheovhedv
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      fhiorfriofnr
-      <br></br>
-      <br></br>
-      bfoiebfoiefne
     </main>
   );
 }
