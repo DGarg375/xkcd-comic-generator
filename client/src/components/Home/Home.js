e7fd1b153816f85edb7a17eb89c052554c459937
DGarg375 <gargdivyansh11@gmail.com>
2022-08-29 18:42:42 +0530

    finished home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return(
         <div className="home-main-container">
             <div className="welcome-box">
                 <div className="welcome-text">WELCOME TO THE xkcd COMIC GENERATOR!</div>
+                <Link to="/comic/random">
+                    <button className="get-comic-home-button">GET RANDOM COMIC</button>
+                </Link>
             </div>
         </div>
     );
