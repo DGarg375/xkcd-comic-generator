6627405cf311a0745cb1d0d4f7f11c14e52cece0
DGarg375 <gargdivyansh11@gmail.com>
2022-08-31 13:46:42 +0530

    completed random comics page

--- client/src/components/Comic/ComicRandom.js
+++ client/src/components/Comic/ComicRandom.js
@@ -1,11 +1,60 @@
 import './ComicRandom.css';
 import { useLoadingContext } from 'react-router-loading';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function ComicRandom() {
+    const [latestComicNum, setLatestComicNum] = useState(2000);
+    const [randomComicNum, setRandomComicNum] = useState(Math.floor(Math.random() * latestComicNum) + 1);
+    const [randomComicData, setRandomComicData] = useState([]);
+
+    function getNewComic() {
+        setRandomComicNum(Math.floor(Math.random() * latestComicNum) + 1);
+    }
+
     const loadingContext = useLoadingContext();
-    loadingContext.done();
+
+    useEffect(() => {
+        axios.get('/getLatest')
+        .then(response => {
+            setLatestComicNum(response.data.num);
+            setTimeout(() => {
+                loadingContext.done(); 
+            }, 2000);
+        }); 
+    }, []);
+
+    useEffect(() => {
+        axios.get(`/getById?id=${randomComicNum}`)
+        .then(response => {
+            setRandomComicData(response.data);
+        });
+    }, [randomComicNum]);
+
     return(
-        <div>this is the random comic page</div>
+        <div className="home-main-container">
+            <div className="main-content">
+                <button type="button" className="randomButtons" onClick={getNewComic}>GET MORE COMICS</button>
+                <ComicImgRender img_url={randomComicData.img}/>
+                <button type="button" className="randomButtons">Details</button>
+            </div>
+            
+        </div>
+    );
+}
+
+function ComicImgRender(props) {
+    return(
+        <div className="test">
+            <img src={props.img_url} alt="Comic Img"></img>
+        </div>
+    );
+}
+
+function ComicDetRender(props) {
+    return(
+        <div>these are comic details</div>
     );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 4000;
 
@@ -8,6 +8,21 @@ const app = express();
 
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/getLatest', (req, res) => {
+    axios.get('https://xkcd.com/info.0.json')
+    .then(response => {
+        res.json(response.data);
+    });
+});
+
+app.get('/getById', (req, res) => {
+    const comic_id = req.query.id;
+    axios.get(`https://xkcd.com/${comic_id}/info.0.json`)
+    .then(response => {
+        res.json(response.data);
+    });
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
