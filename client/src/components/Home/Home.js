3ec3983152286b2b5b0419ad5bf890674fb69b79
DGarg375 <gargdivyansh11@gmail.com>
2022-09-03 12:14:58 +0530

    added background image

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -11,6 +11,7 @@ function Home() {
 
     return(
         <div className="home-main-container">
+        <div className="bgnd-img"></div>
             <div className="welcome-box">
                 <div className="welcome-text">WELCOME TO THE xkcd COMIC GENERATOR!</div>
                 <Link to="/comic/random">
